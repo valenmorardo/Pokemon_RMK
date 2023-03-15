7dc23458ab871eaf005b7b3986ea63516a4e732f
Valentin <valentin.morardo@gmail.com>
2023-03-15 17:52:32 -0300

    components template showPokemones ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
+import HomePokemones from "./components/ShowPokemones/HomePokemones/HomePokemones";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
         <Route exact path="/" Component={LandingPage} />
         <Route path="/home" Component={Home} />
-        <Route exact path="/home/pokemones" /* Component={} */ />
+        <Route exact path="/home/pokemones" Component={HomePokemones} />
         <Route exact path="/home/creation" /* Component={} */ />
         <Route exact path="/home/donation" /* Component={} */ />
         
---
+++ client/src/components/ShowPokemones/HomePokemones/HomePokemones.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+
+
+
+
+
+const HomePokemones = () => {
+  return (
+
+    <div>
+        
+    </div>
+
+  )
+}
+
+export default HomePokemones
