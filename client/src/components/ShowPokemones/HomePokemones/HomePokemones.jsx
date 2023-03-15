218c04e1da58a645c7384ca27089dd795e392b5c
Valentin <valentin.morardo@gmail.com>
2023-03-15 20:55:14 -0300

    pequeña correccion

--- client/src/components/ShowPokemones/HomePokemones/HomePokemones.jsx
+++ client/src/components/ShowPokemones/HomePokemones/HomePokemones.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import { getPokemones } from '../../../redux/actions/index';
-import Cards from '../Cards/Cards';
-import Paginado from '../Paginado/Paginado';
+import React from "react";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getPokemones } from "../../../redux/actions/index";
+import Cards from "../Cards/Cards";
+import Paginado from "../Paginado/Paginado";
 
 const HomePokemones = () => {
-
   const dispatch = useDispatch();
-  const allPokemones = useSelector((state) => state.pokemonesHome) // traigo todos los pokemones del reducer
-
+  const allPokemones = useSelector((state) => state.pokemonesHome); // traigo todos los pokemones del reducer
 
   //PAGINADO
   const [pagina, setPagina] = useState(1);
@@ -18,22 +16,19 @@ const HomePokemones = () => {
   const currentsPokemones = allPokemones.slice(
     (pagina - 1) * porPagina,
     (pagina - 1) * porPagina + porPagina
-    )
-  ////////////////////
+  );
+  //---------------------------------------
 
   useEffect(() => {
     dispatch(getPokemones());
-  }, [dispatch])
-
+  }, [dispatch]);
 
   return (
-
     <div>
-        <Cards pokemones = {currentsPokemones}/>
-        <Paginado pagina={pagina} setPagina={setPagina} maximo={maximo}/>
-    </div> 
+      <Cards pokemones={currentsPokemones} />
+      <Paginado pagina={pagina} setPagina={setPagina} maximo={maximo} />
+    </div>
+  );
+};
 
-  )
-}
-
-export default HomePokemones
+export default HomePokemones;
