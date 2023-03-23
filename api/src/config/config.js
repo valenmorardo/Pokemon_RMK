76c03f2088795a4d751a1f649be78f7069c2f587
Valentin <valentin.morardo@gmail.com>
2023-03-22 22:54:30 -0300

    config dotenv

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
+
+require("dotenv").config();
+
 
 module.exports = {
-    PORT: process.env.PORT || 3001,
-    DB: process.env.DB || `mongodb://127.0.0.1:27017/pokemonRMK`  // --> "pokemonRMK", es el nombre de la base de datos
+    PORT: process.env.PORT,
+    DB: `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`  // --> "pokemonRMK", es el nombre de la base de datos
 }
