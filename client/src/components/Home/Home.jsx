8812fc223caf424d99627298c9d5bffa51262c58
Valentin <valentin.morardo@gmail.com>
2023-04-24 21:43:44 -0300

    home ready, queda hacerlo responsive

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import s from "./Home.module.css"
 
 const Home = () => {
   return (
-    <div>
-        <Link to='/home/pokemones'>
-            <button>VER POKEMONES</button>
+    <div className={s.background}>
+
+      <div className={s.container}>
+        <Link to="/home/pokemones">
+          <button className={s.verPokemones}><span className={s.textButton}>VER POKEMONES </span></button>
         </Link>
 
-        <Link to='/home/creation'>
-            <button>Crear mi pokemon</button>
+        <Link to="/home/creation">
+          <button className={s.create}><span className={s.textButton}>CREATE POKEMON</span></button>
         </Link>
 
-        <Link to='/home/donation'>
-            <button>DONAR</button>
+        <Link to="/home/donation">
+          <button className={s.donate}><span className={s.textButton}>DONAR</span></button>
         </Link>
 
-        <Link to='/home/about'>
-            <button>ABOUT</button>
+        <Link to="/home/about">
+          <button className={s.about}> <span className={s.textButton}>ABOUT</span></button>
         </Link>
 
+      </div>
 
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
