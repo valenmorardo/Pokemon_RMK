b05de6c6bbfd0f7d15ff429836644b65c3c31853
Valentin <valentin.morardo@gmail.com>
2023-05-30 16:33:21 -0300

    comit deploy 1

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 
 module.exports = {
-    PORT: process.env.PORT,
-    DB: `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`  
+    PORT: process.env.MONGOPORT,
+    /* DB: `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`   */
+    DB: process.env.MONGO_URL
 }
+
